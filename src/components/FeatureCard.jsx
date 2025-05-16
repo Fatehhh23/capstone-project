476c3b9c17b57b8ed66e24f1c22c26af87fb189b
Majid <majidhadi02@gmail.com>
2025-05-17 01:53:33 +0700

    Menghapus file dan folder lama dari repositori

--- src/components/FeatureCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function FeatureCard({ icon, title, desc }) {
-  return (
-    <div className="card">
-      <img src={icon} alt={title} />
-      <h3>{title}</h3>
-      <p>{desc}</p>
-    </div>
-  );
-}
